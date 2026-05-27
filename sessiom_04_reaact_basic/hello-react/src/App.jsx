7802aaa4237595d0b171d8ec0b146fef25fa791f
Nguyễn Hải Ban <tonydevhehe@gmail.com>
2026-05-27 10:09:52 +0700

    Bai thuc hanh 4 phong may tinh

---
+++ sessiom_04_reaact_basic/hello-react/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+            <h1>Tên của bạn ở đây</h1>
+            <p>Hôm nay là ngày đẹp trời</p>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+            </ul>
+        </div>
+    </>
+  )
+}
+
+export default App
